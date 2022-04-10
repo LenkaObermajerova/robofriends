3366e968b1f900279b0364cc161d15b58371e067
Lenka Obermajerová <lenka.obermajerova@email.cz>
2022-04-10 22:00:17 +0200

    Space deleting

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,23 +26,23 @@ class App extends Component {
   };
 
   render() {
-    const {robots, searchfield} = this. state;
-    const filterRobots = robots.filter(robot => {
+    const { robots, searchfield } = this.state;
+    const filterRobots = robots.filter((robot) => {
       return robot.name
         .toLocaleLowerCase()
         .includes(searchfield.toLocaleLowerCase());
     });
-    return !robots.length? 
-    <h1 className="tc">Loading</h1> :
-    (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filterRobots} />;
-          </Scroll>
-        </div>
-      );
+    return !robots.length ? (
+      <h1 className="tc">Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filterRobots} />;
+        </Scroll>
+      </div>
+    );
   }
 }
 
