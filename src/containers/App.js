41d5c6285d98ccc3e81e96d28ca8cda4ea8521cf
Lenka Obermajerová <lenka.obermajerova@email.cz>
2022-04-10 21:29:16 +0200

    Improving the app - scrolling - folder structure - simplyfying

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "./App.css";
+import Scroll from "../components/Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filterRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filterRobots} />;
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
